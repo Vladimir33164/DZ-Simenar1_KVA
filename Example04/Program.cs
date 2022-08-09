94bda19dab763c079700ae9240871e970eb53dcd
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-09 20:24:18 +0300

    Домашнее задание

---
+++ Example04/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+
+int a, b, c;
+Console.WriteLine("Ввод a: ");
+a = int.Parse(Console.ReadLine().ToString());
+Console.WriteLine("Ввод b: ");
+b = int.Parse(Console.ReadLine().ToString());
+Console.WriteLine("Ввод c: ");
+c = int.Parse(Console.ReadLine().ToString());
+if (a > b && a > c) // && (Логические И, А ТАКЖЕ) инициирует выполнение инструкций
+{
+    Console.WriteLine($"a = {a}; b = {b}; c = {c} -> max a = {a}"); // -> присваивание
+}
+else
+if (b > a && b > c)
+{
+    Console.WriteLine($"a = {a}; b = {b}; c = {c} -> max b = {b}");
+}
+else
+if (c > a && c > b)
+{
+    Console.WriteLine($"a = {a}; b = {b}; c = {c} -> max c = {c}");
+}
+else
+{
+    Console.WriteLine($"a = {a}; b = {b}; c = {c} -> a = b = c = {a}");
+}
